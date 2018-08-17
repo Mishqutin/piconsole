6612b3bd20f999a186dcc661df5d0b1ebe642d57
Jajko Raspberry Pi <mundzioch@wp.pl>
2018-08-18 00:01:52 +0200

    cmd.php rewrite

--- cmd.php
+++ cmd.php
@@ -1,10 +1,9 @@
 <?php
-if(isset($_POST['status'])){
-        $message = shell_exec("cd /home/pi/ && sudo /var/www/html/test.py 18 ".$_POST['status']." 2>&1");
-} else  {
-	$message = shell_exec("cd /home/pi/ && sudo /var/www/html/test.py 18 test 2>&1");
+if(isset($_POST['command'])){
+        $message = shell_exec("cd /home/pi/ && sudo /var/www/html/cmd.py ".$_POST['command']." 2>&1");
 }
+
 $message = trim(preg_replace('/\s+/', ' ', $message));
-echo json_encode(array('status' =>str_replace('\n','', $message)));
+echo json_encode(array('response' =>str_replace('\n','', $message)));
 
 ?>
