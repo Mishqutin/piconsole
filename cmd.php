b9d0d07736714b38decea3c0997fc9e136fc047b
Waldemar Stanisławski <waldemar@datamomentum.com>
2018-08-18 19:42:42 +0200

    index

--- cmd.php
+++ cmd.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 require_once('helpers.php');
 // require_once('../logs.php');
 
-// var_dump(htmlentities($_POST['command']));
+var_dump(htmlentities($_POST['command']));
 if(isset($_POST['command'])){
         $message = shell_exec("cd /home/pi/ && sudo /var/www/html/piconsole/cmd.py ".get_client_ip()." ".htmlentities($_POST['command'])." 2>&1");
 }
@@ -13,5 +13,5 @@ if(isset($_POST['command'])){
 // $message = trim(preg_replace('/\s+/', ' ', $message));
 // echo json_encode(array('response' =>str_replace('\n','', $message) ));
 
-echo json_encode(array('response' =>$message));
+echo json_encode(array('response' =>htmlentities($message)));
 ?>
