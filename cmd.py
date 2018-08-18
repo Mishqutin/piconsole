03bb152e7997e50264c2c19c7c526d0f25c3ed0a
Michal Stanislawski <mundzioch@wp.pl>
2018-08-18 16:24:34 +0200

    Started doing some real stuff

--- cmd.py
+++ cmd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys, random, os
+import sys, random, os, socket
 from inspect import isclass
 
 invalidCmdWtf = ["Jestes ********", "********* cie", "Noo i ****", "**** wie", "**********", "*********** Ci obore", "Przepraszam, lecz podajac mi takowe oto argumenty rozumiem, iz nie boisz sie, ze wilize ci piety lub wyrzecam ci wlosy lonowe widelcem do ostryg.."]
@@ -61,6 +61,87 @@ class basicCmds:
                 f.close()
                 print(x.replace("\n", "<br>"))
     cmds["cat"] = cmdCat
+
+
+
+
+
+class cscCmds:
+    def cmdExec(line):
+        pass
+
+
+    def client(x):
+        if x[0]=="/":
+            #reconnect(x[1:].split())
+            return 0
+
+        # Connect to host
+        try:
+            s = socket.socket()
+            s.connect(IP)
+        except:
+            print("Could not connect to server")
+            return 1
+
+        s.send(( cscCmds.config["name"]+"@"+cscCmds.config["entryCode"] ).encode())
+        reply = (s.recv(16)).decode("ASCII")
+        if reply!="ok":
+            print("Access denied.")
+            s.close()
+            return 2
+
+
+        # Send command as byte string
+        s.send(x.encode())
+
+        # Loop will end after disconnection
+        while True:
+            try:
+                # Receive text and save as string
+                txt = (s.recv(65536)).decode("ASCII")
+
+                # Receiving empty string means that socket's closed
+                if txt=="": break
+                # Execute requested script from /scripts
+                elif txt[0:3]=="#//":
+                    # Script file name
+                    cmd = txt.split()[0][3:]
+                    # Additional arguments
+                    args = txt.split()[1:]
+
+                    # Script existence check
+                    if os.path.isfile("scripts/{}.py".format(cmd)):
+                        # Load script
+                        f = open("scripts/{}.py".format(cmd), 'r')
+                        code = f.read()
+                        f.close()
+
+                        # Execute it
+                        exec(code, globals())
+                    # Invalid script name
+                    else:
+                        print("Client: Invalid script file")
+                # If received simple string then print it out to console
+                else:
+                    print(txt)
+            # If got any error (mainly by socket being closed) break the loop
+            except: break
+
+        # Disconnect
+        s.close()
+
+
+
+
+
+
+
+
+
+
+
+
 
 class testCmds:
     def cmdExec(line):
