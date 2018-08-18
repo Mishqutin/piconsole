1eafe4f0d9dbc245c9bff49313d05817f6d622ba
Waldemar Stanisławski <waldemar@datamomentum.com>
2018-08-18 19:31:28 +0200

    index

--- cmd.php
+++ cmd.php
@@ -10,7 +10,8 @@ if(isset($_POST['command'])){
         $message = shell_exec("cd /home/pi/ && sudo /var/www/html/piconsole/cmd.py ".get_client_ip()." ".htmlentities($_POST['command'])." 2>&1");
 }
 
-$message = trim(preg_replace('/\s+/', ' ', $message));
-echo json_encode(array('response' =>str_replace('\n','', $message) ));
+// $message = trim(preg_replace('/\s+/', ' ', $message));
+// echo json_encode(array('response' =>str_replace('\n','', $message) ));
 
+echo json_encode(array('response' =>$message));
 ?>
