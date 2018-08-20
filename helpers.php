1e3e88307ccb7f2b6704d20d653c690c8e87a45a
Waldemar Stanisławski <waldemar@datamomentum.com>
2018-08-20 20:37:12 +0200

    login

--- helpers.php
+++ helpers.php
@@ -1,4 +1,13 @@
 <?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login/login.php");
+    exit;
+}
+
 function get_client_ip() {
     $ipaddress = '';
     if (isset($_SERVER['HTTP_CLIENT_IP']))
